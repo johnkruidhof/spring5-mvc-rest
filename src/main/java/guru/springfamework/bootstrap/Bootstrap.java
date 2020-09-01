c6eafbe99b967a4faaa6cd5b41eeed052e222bb0
johnkruidhof <johnkruidhof@gmail.com>
2020-09-01 15:27:11 +0200

    Init

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -1,15 +1,9 @@
 package guru.springfamework.bootstrap;
 
-import guru.springfamework.domain.Category;
 import guru.springfamework.repositories.CategoryRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
-/**
- * Created by jt on 9/24/17.
- */
-@Component
-public class Bootstrap implements CommandLineRunner{
+public class Bootstrap implements CommandLineRunner {
 
     private CategoryRepository categoryRepository;
 
@@ -19,29 +13,6 @@ public class Bootstrap implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        Category fruits = new Category();
-        fruits.setName("Fruits");
-
-        Category dried = new Category();
-        dried.setName("Dried");
-
-        Category fresh = new Category();
-        fresh.setName("Fresh");
-
-        Category exotic = new Category();
-        exotic.setName("Exotic");
-
-        Category nuts = new Category();
-        nuts.setName("Nuts");
-
-        categoryRepository.save(fruits);
-        categoryRepository.save(dried);
-        categoryRepository.save(fresh);
-        categoryRepository.save(exotic);
-        categoryRepository.save(nuts);
-
-
-        System.out.println("Data Loaded = " + categoryRepository.count() );
 
     }
 }
